98f4b1129839c01c15f92b36c923644e8812ef97
TheNanoBay <noorani.mehdi@outlook.com>
2021-02-04 08:09:42 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+  //first we grab the form .... so that we can listen for the submit button 
+  const form = document.getElementById("create-task-form")
+
+
+  //listens for the submit button to be pushed 
+  form.addEventListener('submit', logSubmit);
+
+
+  //when we fire our submit button....we want to do these steps 
+  function logSubmit(event){
+    event.preventDefault();
+
+    const userInput = document.getElementById("new-task-description").value;
+
+    const tasks = document.getElementById("tasks");
+
+    const newTask = document.createElement('li');
+    newTask.innerText = userInput;
+
+    tasks.appendChild(newTask);
+
+  }
+
 });
